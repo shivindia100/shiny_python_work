786715c26eab6b399dd5738149dadcc4ce5bf94e
Shivendra Kumar <shiv.india100@gmail.com>
2025-08-08 22:55:35 +0530

    Added Challenge-2 solution

--- mini_challenges/Challenge1-env_var_reader.py
+++ mini_challenges/Challenge1-env_var_reader.py
@@ -1,1 +1,14 @@
-import dotenv
+# import dtenv is a module that content function to load the dotenv file we can also write
+from dotenv import load_dotenv
+#import dotenv
+import os
+
+#dotenv.load_dotenv()
+load_dotenv()
+API_KEY = os.getenv("MY_API_KEY")
+# if key is not found then it returns the none
+
+if API_KEY:
+    print("API is loaded successfully")
+else:
+    print("API is not loaded")
